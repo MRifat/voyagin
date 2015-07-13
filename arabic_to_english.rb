45cf404c9f89a1e3b413b10d5746f1a8a5af09c2
Rifat <mrifat.hammad@gmail.com>
2015-07-13 03:30:42 +0300

    Converting Tens

--- arabic_to_english.rb
+++ arabic_to_english.rb
@@ -3,6 +3,8 @@ class ArabicToEnglish
   attr_reader :number
 
   ONES = { 0 => "Zero", 1 => "One", 2 => "Two", 3 => "Three", 4 => "Four", 5 => "Five", 6 => "Six", 7 => "Seven", 8 => "Eight", 9 => "Nine" }
+  TENS = { 10 => "Ten", 11 => "Eleven", 12 => "Twilve", 13 => "Thirteen", 14 => "Fourteen", 15 => "Fifteen", 16 => "Sixteen", 17 => "Seventeen", 18 => "Eighteen",
+           19 => "Nineteen", 20 => 'Twinty', 30 => 'Thirty', 40 => 'Fourty', 50 => 'Fifty', 60 => 'Sixty', 70 => 'Seventy', 80 => 'Eighty', 90 => 'Ninety' }
 
   def initialize(number)
     @number ||= number
@@ -11,6 +13,11 @@ class ArabicToEnglish
   def convert
     if @number / 10 == 0
       return ONES[@number]
+    elsif @number / 10 == 1
+      return TENS[@number]
+    elsif @number / 10 == 2
+      digits = @number.to_s.split('').map(&:to_i)
+      return digits.last.zero? ? TENS[@number] : TENS[20] << " #{ONES[digits.last]}"
     end
   end
 end
--- spec/arabic_to_english_spec.rb
+++ spec/arabic_to_english_spec.rb
@@ -7,5 +7,26 @@ describe ArabicToEnglish do
       a_2_e = ArabicToEnglish.new(1)
       expect(a_2_e.convert).to eq('One')
     end
+
+    it "should return Ten when when calling convert with 10" do
+      a_2_e = ArabicToEnglish.new(10)
+      expect(a_2_e.convert).to eq('Ten')
+    end
+
+    it "should return Fifteen when when calling convert with 15" do
+      a_2_e = ArabicToEnglish.new(15)
+      expect(a_2_e.convert).to eq('Fifteen')
+    end
+
+    it "should return Twinty when when calling convert with 20" do
+      a_2_e = ArabicToEnglish.new(20)
+      expect(a_2_e.convert).to eq('Twinty')
+    end
+
+    it "should return Twinty Five when when calling convert with 25" do
+      a_2_e = ArabicToEnglish.new(25)
+      expect(a_2_e.convert).to eq('Twinty Five')
+    end
+
   end
 end
